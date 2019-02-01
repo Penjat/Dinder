fd3bbb4a507716f44a201680373ada41451ef89c
Penjat <theSpencerSymington@Gmail.com>
2019-01-31 18:15:06 -0800

    overriding UIView init

--- Dinder/ViewControllers/EventView.swift
+++ Dinder/ViewControllers/EventView.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class EventView: UIView {
 
-    
-
+  override init(frame: CGRect) {
+    super.init(frame:frame)
+    let eventTableView = UITableView()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
 }
