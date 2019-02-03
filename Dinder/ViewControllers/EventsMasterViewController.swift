f5af887fa443bc4b1d23a19dbd2c385250b689ac
Penjat <theSpencerSymington@Gmail.com>
2019-02-03 12:44:13 -0800

    displays a no events message if array count == 0, different message for different sorts, msg fades in

--- Dinder/ViewControllers/EventsMasterViewController.swift
+++ Dinder/ViewControllers/EventsMasterViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class EventsMasterViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
   
   var mainUser: User?
+  var eventsToShow = [Event]()
+  var showingMyEvents = true //true is my events, false is events I am interested in
   
   @IBOutlet weak var noEventsMsg: UIView!
+  @IBOutlet weak var noEventsMsgLabel: UILabel!
   
   @IBOutlet weak var eventTableView: UITableView!
   
@@ -22,13 +25,15 @@ class EventsMasterViewController: UIViewController,UITableViewDataSource,UITable
     eventTableView.delegate = self
     
     //TODO check the events
-    noEventsMsg.isHidden = true
+    
     
   }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     
+    //if no events show with message
+    noEventsMsg.isHidden = (eventsToShow.count > 0)
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,7 +49,7 @@ class EventsMasterViewController: UIViewController,UITableViewDataSource,UITable
     return 2
   }
   func numberOfSections(in tableView: UITableView) -> Int {
-    return 0
+    return eventsToShow.count
   }
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     if indexPath.row == 0{
@@ -52,5 +57,24 @@ class EventsMasterViewController: UIViewController,UITableViewDataSource,UITable
     }
     return 250
     
+  }
+  @IBAction func switchToMyEvents(_ sender: Any) {
+    //TODO put in separet function
+    showingMyEvents = true
+    noEventsMsgLabel.alpha = 0.0
+    noEventsMsgLabel.text = "you have created no events"
+    UIView.animate(withDuration: 1.0, animations: {
+      self.noEventsMsgLabel.alpha = 1.0
+    })
+    
+  }
+  @IBAction func switchToInterestedEvents(_ sender: Any) {
+    //TODO put in separet function
+    showingMyEvents = false
+    noEventsMsgLabel.alpha = 0.0
+    noEventsMsgLabel.text = "you are not intereste in any events"
+    UIView.animate(withDuration: 1.0, animations: {
+      self.noEventsMsgLabel.alpha = 1.0
+    })
   }
 }
