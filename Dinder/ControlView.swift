19ae7281ce1736501fb96bc2b18f1d2cd27fb6df
Penjat <theSpencerSymington@Gmail.com>
2019-02-02 16:53:06 -0800

    connected all outlets

--- Dinder/ControlView.swift
+++ Dinder/ControlView.swift
@@ -5,8 +5,18 @@ import UIKit
 class ControlView: UIView {
   
   @IBOutlet var contentView: UIView!
+  @IBOutlet weak var eventTitleLabel: UITextField!
+  @IBOutlet weak var eventDescription: UITextView!
+  @IBOutlet weak var eventDatePicker: UIDatePicker!
+  @IBOutlet weak var eventLocationField: UITextField!
+  @IBOutlet weak var guestGenderToggle: UISegmentedControl!
+  @IBOutlet weak var relationshipTypeToggle: UISegmentedControl!
+  @IBOutlet weak var totalCostField: UITextField!
+  @IBOutlet weak var costToggle: UISegmentedControl!
   
-  override init(frame: CGRect) {
+  
+  
+    override init(frame: CGRect) {
     super.init(frame: frame)
     commonInit()
   }
