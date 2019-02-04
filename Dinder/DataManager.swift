98da8da928615dac950a125d97ebe4da8bf620e6
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-02-03 20:05:50 -0800

    firebase being updated

--- Dinder/DataManager.swift
+++ Dinder/DataManager.swift
@@ -40,6 +40,42 @@ class DataManager{
         
         // update data model
         
+        let emailOfHost = event.owner.emailAddress
+        
+        
+
+        let keyworthyEmail = emailOfHost.replacingOccurrences(of: ".", with: "__DOT__")
+        
+        let startDateTime = event.startDateTime
+        
+    
+        
+        var formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let defaultTimeZoneStr = formatter.string(from: startDateTime)
+        
+        // firebase key is in format of:
+        // email__DOT__COM~yyyy-mm-dd hh:mm:ss
+        var key: String = keyworthyEmail
+        key.append("~")
+        key.append(defaultTimeZoneStr)
+        
+        print("THE KEY IS \(key)")
+        
+        
+        
+        
+        let usersRef = Database.database().reference(withPath: "events")
+        let amyRef = usersRef.child(key)
+        let amyBdRef = amyRef.child("emailaddress")
+        amyBdRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            print("GOT IT \(snapshot.value ?? "unknown email")!")
+        })
+        //  amyBdRef.setValue("1999-12-31")
+        
+       // return users[0]
+        
+        
         
     }
     
@@ -49,10 +85,63 @@ class DataManager{
         print("\(user.firstName) not interested in \(event.title)")
     }
     
+    
+    
+    
     func getNextEvent(filters: [String:String]) -> Event{
+        
+        currentEventNum += 1
+        currentEventNum %= self.events.count
+        
+        let usersRef = Database.database().reference(withPath: "events")
+        
+        usersRef.observeSingleEvent(of: .value, with: { (snapshot) in
+        
+           
+            
+            if let allEvents = snapshot.value as? [String:Any] {
+                for (_,event) in allEvents {
+                    print("eeeeeee: \(event)");
+                    
+                    for(theKey, field) in (event as? [String:Any])!{
+                        print("field is \(field) for key \(theKey)")
+
+                    }
+                    
+                    
+                   // let userInterest = profile["Interest"]
+                }
+            }
+            
+            
+           // let value = snapshot as? [NSDictionary]
+            print("VALUE OF KEY IS \(snapshot.key)!")
+           // print("found next event: \(title)")
+        
+        })
+        
+        return events[currentEventNum];
+        
+        
+    }
+    
+    
+    
+    
+    
+    func getNextEventSpencer(filters: [String:String]) -> Event{
         currentEventNum += 1
         let _: User = getUserFromFirebase(userId: 12)
         return self.events[currentEventNum % events.count] // need dictionary of options, plus keep track of current event
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
     
@@ -97,32 +186,24 @@ class DataManager{
         }
     }
     
+    
+    
+    
     func getUserFromFirebase(userId: Int) -> User{
         
         let usersRef = Database.database().reference(withPath: "users")
         let amyRef = usersRef.child("amy@tang__DOT__com")
         let amyBdRef = amyRef.child("birthdate")
         amyBdRef.observeSingleEvent(of: .value, with: { (snapshot) in
-            //   print("snapshot value is \(String(describing: snapshot.value))")
-            
-            
             print("GOT IT \(snapshot.value ?? "unknown birthdate")!")
-            
-            
-        
-            
-//            var eventKey = "events/alice@leal__DOT__com~2019-02-14 06:56:30"
-//            print("one value is \(snapshot.value)")
-//
-//
-//            if let userDict = snapshot.value as? [String : Any] {
-//                // print(userDict.debugDescription)
-//            }
         })
-        amyBdRef.setValue("1999-12-31")
+      //  amyBdRef.setValue("1999-12-31")
         
         return users[0]
     }
+    
+    
+    
     
     
     func getOwnerOf(event: Event) -> User{
